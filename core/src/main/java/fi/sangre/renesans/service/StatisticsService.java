3bbb412548680ddeb20dd7286ab1f80ff4aa5c10
Artem Antcev <artem@tx.company>
2021-08-11 12:40:31 +0300

    [Aftergame][VAI]: removed the wrong method call from calculation of VAI

--- core/src/main/java/fi/sangre/renesans/service/StatisticsService.java
+++ core/src/main/java/fi/sangre/renesans/service/StatisticsService.java
@@ -416,7 +416,7 @@ public class StatisticsService {
         final List<CatalystStatistics> enabledCatalysts = findEnabledCatalysts(catalysts);
         final double ratio = enabledCatalysts.size() > 0 ? enabledCatalysts.size() : 1d;
 
-        return rateToPercent(catalysts.stream().mapToDouble(CatalystStatistics::getResult).sum() / ratio);
+        return catalysts.stream().mapToDouble(CatalystStatistics::getResult).sum() / ratio;
     }
 
     private Map<Question, StatisticsAnswer> getAnswerStatistics(final Set<Question> questions, final List<Respondent> respondents) {
